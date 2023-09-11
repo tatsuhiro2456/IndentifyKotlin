73123bee54592351059272a7e09f147a6fa86d45
tatsuhiro2456 <tatsuhiro2456@gmail.com>
2023-09-12 01:16:27 +0900

    Initial commit

---
+++ app/src/main/java/com/example/jetprofile/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.example.jetprofile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.jetprofile.ui.theme.JetProfileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetProfileTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        //名前
+                        Text(
+                            text = "松原　立洋",
+                            color = Color.Gray,
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.ExtraBold,
+                        )
+
+                        //空の行
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        //職業
+                        Text(
+                            text = "職業：大学院生",
+                            color = Color.Gray,
+                            fontSize = 16.sp,
+                        )
+                    }
+
+                }
+            }
+        }
+    }
+}
